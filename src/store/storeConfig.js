afeaa85cf1a74b73ee821e7f569546dd9da57b52
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-31 15:50:23 -0300

    Invocando Action Creator #01

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,15 +1,26 @@
 import { legacy_createStore as createStore, combineReducers} from 'redux'
 
 const reducers = combineReducers({
-    numeros: function(state, action) {
-         console.log(state, '', action )
-        return {
-            min: 7,
-            max: 31,
+    numeros: function(state, action) { 
+        switch(action.type){
+            case 'NUM_MIN_ALTERADO':
+                return {
+                    ...state,
+                    min: action.payload 
+                }
+            default:
+                 return {
+                    min: 7,
+                    max: 31,
+                }     
         }
+
+             
+      
     },
     nomes: function(state, action) {
-        console.log(state, '', action )
+        console.log('Reducer Nomes...')
+         console.log(state, '', action )
         return[
             'Ana',
             'Bia',
