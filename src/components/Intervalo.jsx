cb752bcf7ed14fee488e5fc819ce50c06fd5066d
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-31 15:49:41 -0300

    Invocando Action Creator #01

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,34 +1,46 @@
 import "./Intervalo.css";
 import React from "react";
 import Card from "./Card";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
+import { alterarNumeroMinimo } from "../store/actions/numeros";
 
-function Intervalo (props) {
-    const {min, max} = props
+function Intervalo(props) {
+  const { min, max } = props;
+  props.alterarMinimo(10000)
+  
+
   return (
     <Card title="Intervalo de números" red>
       <div className="Intervalo">
         <span>
           <strong>Mínimo</strong>
-          <input type="number" value={min} readOnly
-             />
+          <input type="number" value={min} readOnly />
         </span>
         <span>
           <strong>Máximo</strong>
-          <input type="number" value={max} readOnly
-              />
+          <input type="number" value={max} readOnly />
         </span>
       </div>
     </Card>
   );
-};
-
-function mapStateToProp(state){
-   return{
-    min: state.numeros.min,
-    max: state.numeros.max,
-   }
-
 }
 
-export default connect(mapStateToProp) (Intervalo)
+function mapStateToProp(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+function mapActionCreatorsToProp(dispatch) {
+  return {
+    alterarMinimo(novoNumero) {
+      // action creator ->(retona) action
+      const action = alterarNumeroMinimo(novoNumero);
+      dispatch(action);
+    },
+  };
+}
+
+/* fazer na ordem */
+export default connect( mapStateToProp, mapActionCreatorsToProp)(Intervalo);
